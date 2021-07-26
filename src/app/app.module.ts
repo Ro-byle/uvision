804edcc88fc4a784aecb866a9be194be2ed94c85
Ro-byle <robelhaswag@gmail.com>
2021-07-26 11:02:01 -0700

    Trying to install dependecies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,16 @@ import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
+import { WebView } from '@ionic-native/ionic-webview/ngx';
+import { File, FileEntry } from '@ionic-native/file/ngx';
+// import { File, FileEntry } from '@ionic-native/File/ngx';
+import {
+  Camera,
+  CameraOptions,
+  PictureSourceType
+} from '@ionic-native/camera/ngx';
+import { FilePath } from '@ionic-native/file-path/ngx';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
@@ -13,7 +23,13 @@ import { ProfilepageComponent } from './profilepage/profilepage.component';
   declarations: [AppComponent, PostsComponent, ProfilepageComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy,  }],
-  bootstrap: [AppComponent],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    WebView,
+    Camera,
+    File,
+    FilePath
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
