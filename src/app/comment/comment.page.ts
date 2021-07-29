3666dab28d8ee8052642865ebd0e9d179a32672f
Ro-byle <robelhaswag@gmail.com>
2021-07-29 00:09:51 -0700

    3 files modified

---
+++ src/app/comment/comment.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comment',
+  templateUrl: 'comment.page.html',
+  styleUrls: ['comment.page.scss']
+})
+export class CommentPage {
+  constructor() {}
+
+}
